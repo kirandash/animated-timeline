77ac4814e2acc8dceb14ed482081697ebc2a4237
thekirankumardash <thekirankumardash@gmail.com>
2016-03-17 20:39:55 +0530

    1.Adjusting the layout with jQuery

--- includes/timeline.js
+++ includes/timeline.js
@@ -1,4 +1,25 @@
 /*  JavaScript Document                      */
 
+var timelineWidth = 0;
+var panelWidth = 0;
+var firstRun = true;
+var totalPanels = 0;
+var currentPanel = 0;
+
+$(document).ready(function() {
+    
+	panelWidth = $('.timeline .panel').width();
+	
+	adjustLayout();
+	
+});
+
+function adjustLayout(){
+	
+	$('.timeline .panel').each(function(index) {
+        var newX = panelWidth * index;
+		$(this).css('left', newX+'px');
+    });
+}
 
 
